f85d7ea430732ec43bcf692802b7ea48ed39c0aa
adv3429 <adv3429@rit.edu>
2022-02-03 17:10:54 -0500

    Created Hero subclasses

---
+++ src/heroes/Warrior.java
@@ -0,0 +1,23 @@
+package heroes;
+
+/**
+ * The Warrior class
+ *
+ * @author myname
+ */
+public class Warrior extends Hero{
+
+    // fields
+
+    // constructor
+
+    /**
+     * THe constructor class for Hero
+     *
+     * @param name      the name of the Hero
+     * @param hitPoints the hit points of the Hero
+     */
+    public Warrior(String name, int hitPoints) {
+        super(name, hitPoints);
+    }
+}
---
+++ src/heroes/Wizard.java
@@ -0,0 +1,16 @@
+package heroes;
+
+/**
+ * @author myname
+ */
+public class Wizard extends Hero{
+    /**
+     * THe constructor class for Hero
+     *
+     * @param name      the name of the Hero
+     * @param hitPoints the hit points of the Hero
+     */
+    public Wizard(String name, int hitPoints) {
+        super(name, hitPoints);
+    }
+}
